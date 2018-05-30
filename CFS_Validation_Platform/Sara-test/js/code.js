71903df6815e09bd93da3034b59c7710e3606142
sarag <sara129715@ohio.edu>
2018-05-30 12:37:03 -0400

    Added test files to my test environment Changed the log function to display delimiter

---
+++ CFS_Validation_Platform/Sara-test/js/code.js
@@ -0,0 +1,55 @@
+//Global variables
+var inputData = [];
+
+config = {
+		delimiter: "",	// auto-detect
+		newline: "",	// auto-detect
+		quoteChar: '"',
+		escapeChar: '"',
+		header: false,
+		trimHeader: false,
+		dynamicTyping: false,
+		preview: 0,
+		encoding: "",
+		worker: true,
+		comments: false,
+		step: undefined,
+		complete: undefined,
+		error: undefined,
+		download: false,
+		skipEmptyLines: true,
+		chunk: undefined,
+		fastMode: undefined,
+		beforeFirstChunk: undefined,
+		withCredentials: undefined
+	}
+
+//binding the event listener to the file picker button
+$(document).ready(function(e) {
+	document.getElementById('file').addEventListener('change', readFile, false);
+});
+
+//file reader based on papaparse when the file picker is clicked
+function readFile (evt) {
+    var files = evt.target.files;
+    var file = files[0]; 
+    Papa.parse(file, {
+    	complete: function(results) {
+    		log(results);
+    	}
+    }, config);
+ }
+
+//processing the input and displaying the results
+function log(input){
+	inputData = input.data;
+	$("#log-contect").empty();
+	var html = '<p>Errors <br>';
+	html += input.errors+'</p>';
+	html += '<p>Header <br>';
+	html += input.data[0]+'</p>';
+	html += '<p>Meta <br>';
+	html += input.meta.delimiter+'</p>';
+	$("#log-contect").append(html);
+	console.log(input)
+}
