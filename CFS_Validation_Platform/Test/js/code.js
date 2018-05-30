1731c5c7fb77500f452ba86e2e6c5640cbfc78d6
saeedt <saeed.tehrani@gmail.com>
2018-05-30 00:15:03 -0400

    Cleaned up the shared libraries folder Added more features to the test code

--- CFS_Validation_Platform/Test/js/code.js
+++ CFS_Validation_Platform/Test/js/code.js
@@ -1,7 +1,35 @@
+//Global variables
+var inputData = [];
+
+config = {
+		delimiter: "",	// auto-detect
+		newline: "",	// auto-detect
+		quoteChar: '"',
+		escapeChar: '"',
+		header: false,
+		trimHeader: false,
+		dynamicTyping: false,
+		preview: 0,
+		encoding: "",
+		worker: true,
+		comments: false,
+		step: undefined,
+		complete: undefined,
+		error: undefined,
+		download: false,
+		skipEmptyLines: true,
+		chunk: undefined,
+		fastMode: undefined,
+		beforeFirstChunk: undefined,
+		withCredentials: undefined
+	}
+
+//binding the event listener to the file picker button
 $(document).ready(function(e) {
 	document.getElementById('file').addEventListener('change', readFile, false);
 });
 
+//file reader based on papaparse when the file picker is clicked
 function readFile (evt) {
     var files = evt.target.files;
     var file = files[0]; 
@@ -9,15 +37,17 @@ function readFile (evt) {
     	complete: function(results) {
     		log(results);
     	}
-    });
+    }, config);
  }
 
+//processing the input and displaying the results
 function log(input){
+	inputData = input.data;
 	$("#log-contect").empty();
 	var html = '<p>Errors <br>';
 	html += input.errors+'</p>';
-	html += '<p>Data <br>';
-	html += input.data.length+'</p>';
+	html += '<p>Header <br>';
+	html += input.data[0]+'</p>';
 	html += '<p>Meta <br>';
 	html += input.meta+'</p>';
 	$("#log-contect").append(html);
