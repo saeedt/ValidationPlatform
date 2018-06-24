94f218f7712373b320701d9b82424fcb3403affc
sa129715 <sa129715@ohio.edu>
2018-06-23 22:11:55 -0400

    functions for look up tables are added

--- CFS_Validation_Platform/Sara-test/js/code.js
+++ CFS_Validation_Platform/Sara-test/js/code.js
@@ -42,8 +42,18 @@ $(document).ready(function(e) {
 	console.log(lengthRange("25368544", "zipcode", conf2))
 	console.log(lengthRange("2536854422222", "faxnum", conf2))
 	
-	*/
+	
 	console.log(lkup6( "4231","Belgium", "BE"))
+
+	console.log(lkup("00501","Holtsville","NY")
+	
+	console.log(lkup("Florida","FL")
+		
+	console.log(lkup("Belgium","7210","Ak")	
+			)
+			
+	console.log(lkup("2"))
+	*/
 	document.getElementById('file').addEventListener('change', readFile, false);
 });
 
@@ -74,6 +84,7 @@ function log(input){
 }
 
 //configuration object for verification functions
+/*
 var conf1= {
 	email: {
 		allowed: /[^a-zA-Z0-9!@#$%&'*+-/=?^_`{|}~.]/g,
@@ -84,31 +95,32 @@ var conf1= {
 		allowed: /[^a-zA-Z0-9 _.-]/g,
 		not_allowed: /(P_O_|P.O_|P..B|P.O_|P.O.|PO_B|PO_D|POB_|POST)/g,
 		required: /^.*/
+/*
 	},
 	phone:{
 		allowed: /[^0-9+$]/g,
-		not_allowed: /^.*/,
-		required:/^\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$/g,
-	},
-	
+		//not_allowed: /^.*/ 
+	//	required:/^\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$/g,
+	//},
+	/*
 	numeric:{
 		allowed: /[^0-9+$]/g,
-		not_allowed: /^.*/,
-		required:/^.*/
-	},
-	alphabetic:{
-		allowed: /[^A-Za-z+$ ]/g,
-		not_allowed: /^.*/,
-		required:/^.*/
-	},
-	alphnumeric:{
-		allowed: /[^0-9a-zA-Z+$,():;<>[\ ]]/g,
-		not_allowed: /^.*/,
-		required:/^.*/
-	}
-}
+		not_allowed: /^.*/
+		//required:/^.*/
+	//},
+	//alphabetic:{
+		//allowed: /[^A-Za-z+$ ]/g,
+		//not_allowed: /^.*/,
+		//required:/^.*/
+	//},
+	//alphnumeric:{
+	//	allowed: /[^0-9a-zA-Z+$,():;<>[\ ]]/g,
+		//not_allowed: /^.*/,
+		//required:/^.*/
+	//} 
+//}
 
-
+/*
 
 //error flags object
 var flags = {
@@ -213,7 +225,7 @@ var conf2= {
        return false;  	
       }  
 }
- /*
+
  
 
 
@@ -236,7 +248,7 @@ function isInteger(x) {
 // look up tables 
 
 // define config for look up tables
-
+/*
 var conf3:{
 	lookup1: {
 		sheets rows header... ,
@@ -246,38 +258,267 @@ var conf3:{
 }
 
 */
-function lkup6(zipecode, city, state){
-	var workbook = new Excel.Workbook();
-	workbook.xlsx.readfile("C:\Users\sa129715\git\ValidationPlatform\CFS_Validation_Platform\Sara-test\js\LKUP.xlsx");
-	var worksheet = workbook.getWorksheet("LKUP6");
-	var check;
+
+
+/*
+var conf3= 
+{ "lkup4" :[
+		{
+		"zipeCode": "00501" ,
+		"city": "Holtsville", 
+		"state": "NY" 
+			}, 
+			{
+		"zipeCode": "00544", 
+		"city": "Holtsville", 
+		"state": "NY", 
+			}, 
+			{
+		"zipeCode": "01001", 
+		"city": "Agawam", 
+		"state": "MA", 
+			},
+			{
+		"zipeCode": "01002", 
+		"city": "Amherst", 
+		"state": "MA", 
+			},
+			{
+		"zipeCode": "01003", 
+		"city": "Amherst", 
+		"state": "MA", 
+			}
+			]
+		
+}
+console.log(object.keys(conf3[lkup4]));
+
+*/
+
+// lookup6
+
+
+var lkup6 = [
+    		{
+    	"zipeCode": "00501" ,
+    	"city": "Holtsville", 
+    	"state": "NY" 
+    		}, 
+    		{
+    	"zipeCode": "00544", 
+    	"city": "Holtsville", 
+    	"state": "NY", 
+    		}, 
+    		{
+    	"zipeCode": "01001", 
+    	"city": "Agawam", 
+    	"state": "MA", 
+    		},
+    		{
+    	"zipeCode": "01002", 
+    	"city": "Amherst", 
+    	"state": "MA", 
+    		},
+    		{
+    	"zipeCode": "01003", 
+    	"city": "Amherst", 
+    	"state": "MA", 
+    		}
+    ]
+   
+
+
+
+
+
+
+	
+	// lookup table function for LKUp6
+	
+function lkup(a,b,c){
 	var a;
 	var b;
 	var c;
-	var i;
-	for (i = 2; i <= worksheet['!range'].e.r; i++)
-	{
-		a = worksheet.Cells(i,1).Value;
-		b = worksheet.Cells(i,2).Value;
-		c = worksheet.Cells(i,3).Value;
-		if (a == zipecode&& b == city && c == state)
-		{  	
-			check = 1;
+	var check;
+	check = 0;
+	for (i = 0; i < lkup6.length; i++){
+		if (a == lkup6[i].zipeCode) {
+			check = 1
+			if (b == lkup6[i].city) {
+				if (c == lkup6[i].state) {
+					return true;
+				}
+				else {
+					console.log("State not found!");
+				}
 			}
-			else
-			{  	  		
-			check = 0;  	
-		} 
-	}
-	if (check == 1)
-	{  	
-		return true;
+			else {
+				console.log("City not found!");
+			}
 		}
-		else
-		{  	  		
-		return false;  	
-	} 
+	}
+	if (check == 0){
+		console.log("Zipcode not found!");
+	}
+}
+	
+// function for lkup4
+var lkup4 = [
+	{
+		"state": "Alabama" ,
+		"postalCode": "AL", 
+ 	}, 
+	{
+ 		"state": "Alaska" ,
+ 		"postalCode": "AK", 
+	},
+	{
+		"state": "Arizona" ,
+		"postalCode": "AR", 
+	},
+	{
+		"state": "California" ,
+		"postalCode": "CA", 
+			 
+	},
+	{
+		"state": "Florida" ,
+		"postalCode": "FL", 
+			 
+	}
+]
+
+function lkup(a,b){
+	var a;
+	var b;
+	var check;
+	check = 0;
+	for (i = 0; i < lkup4.length; i++){
+		if (a == lkup4[i].state) {
+			check = 1
+			if (b == lkup4[i].postalCode) {
+				return true;
+			}
+				else {
+					console.log("Postal code not matched with state!");
+				}
+			
+				}
+	}
+	if (check == 0){
+		console.log("state is not found!");
+	}
+}
+// look up 5
+
+var lkup5 = [
+    		{
+    		"name": "Belgium" ,
+    		"countryCode": "5310", 
+    		"isoCode": "AF" 
+    		}, 
+    		{
+    		"name": "Albania" ,
+    		"countryCode": "4810", 
+    		"isoCode": "AL" 
+    	    }, 
+    	    {
+    		"name": "bahrain" ,
+    	  	"countryCode": "5250", 
+    	    "isoCode": "BH" 
+    	   	}, 	
+    	   	{
+        	"name": "Colombia" ,
+        	"countryCode": "3010", 
+        	"isoCode": "Co" 
+        	    		}, 
+    ]
+   
+
+
+	
+	// lookup table function for LKUp6
+	
+function lkup(a,b,c){
+	var a;
+	var b;
+	var c;
+	var check;
+	check = 0;
+	for (i = 0; i < lkup5.length; i++){
+		if (a == lkup5[i].name) {
+			check = 1
+			if (b == lkup5[i].countryCode) {
+				if (c == lkup5[i].isoCode) {
+					return true;
+				}
+				else {
+					console.log("Iso code is not found!");
+				}
+			}
+			else {
+				console.log("country code is not found!");
+			}
+		}
+	}
+	if (check == 0){
+		console.log("Name of country is not found!");
+	}
+}
+
+// lookup 3
+var lkup3 = ["1","2","3","11","24", "244"]
+	
+	function lkup(a){
+		var a;
+		var check;
+		check = 0;
+		for (i = 0; i < lkup3.length; i++){
+			if (a == lkup3[i]) {
+				return true;
+				}
+				else {
+					
+				console.log("mode is not correct!");
+					}
+		}
 }
 
 
-    
+// binary search in general
+var a = [
+    0,
+    13,
+    22,
+    19,
+    5,
+    55,
+    98,
+    41,
+    81,
+    72,
+    68
+];
+a.sort(function(a, b) {
+    return a - b;
+});
+console.log('a,', a);
+console.log(binarySearch(a, 41))
+
+function binarySearch(arr, target) {
+    lo = 1;
+    hi = arr.length;
+    while (lo < hi) {
+    	mid = lo + Math.floor((hi - lo)/2);
+    	if (arr[mid] == target){
+    		console.log(mid, target);
+    		lo = hi + 1;
+    	}
+    	else if (arr[mid] < target) {
+    		lo = mid;
+    	}
+    	else {
+    		hi = mid;
+    	}
+    }
+}
