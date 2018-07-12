3d51df5eecfb443c217e2afe32df31e691c471b3
hodarahmani <hodarahmani@10.233.84.179>
2018-07-12 19:48:35 -0400

    Created edit flags for shipment attributes.

--- CFS_Validation_Platform/Hoda-test/js/code.js
+++ CFS_Validation_Platform/Hoda-test/js/code.js
@@ -791,7 +791,7 @@ var flags = {
 		msg: "Please provide an export mode."
 	},	
 };
-
+};
 //returns true if the string only has the allowed characters
 function check_allowed_char(input,type,config){
 	var filter = eval(config)[type].allowed;
