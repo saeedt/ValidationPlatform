41b2ac9ed3206d13c4a668ac9d7b844c19adbaff
saeedt <saeed.tehrani@gmail.com>
2018-07-16 19:17:35 -0400

    Added binary and exhaustive search functions added all my functions to the test folder

--- CFS_Validation_Platform/Saeed-test/js/code.js
+++ CFS_Validation_Platform/Saeed-test/js/code.js
@@ -53,7 +53,9 @@ function log(input){
 	$("#log-contect").append(html);
 	console.log(input)
 	//check_req_char("m.e@test.com","email","conf1");
-	check_allowed_char("PtO1?", "shippingAdress", "conf1");
+	//check_allowed_char("PtO1?", "shippingAdress", "conf1");
+	//console.log(lkup_exhaustive("city_state_zip","zip","01002"));
+	console.log(lkup_binary("city_state_zip","zip","01009"));
 }
 
 //configuration object for verification functions
@@ -67,7 +69,22 @@ var conf1= {
 		allowed: /[^a-zA-Z0-9 _.-]/g,
 		not_allowed: /(P_O_|P.O_|P..B|P.O_|P.O.|PO_B|PO_D|POB_|POST)/g,
 		required: /^.*/
-	}
+	},
+	numeric:{
+		allowed: /[^0-9+$]/g,
+		not_allowed: /^.*/,
+		required:/^.*/
+	},
+	alphabetic:{
+		allowed: /[^A-Za-z+$ ]/g,
+		not_allowed: /^.*/,
+		required:/^.*/
+	},
+	alphnumeric:{
+		allowed: /[^0-9a-zA-Z+$,():;<>[\ ]]/g,
+		not_allowed: /^.*/,
+		required:/^.*/
+	}	
 }
 //error flags object
 var flags = {
@@ -101,4 +118,69 @@ function check_req_char(input, type, config){
 	var filter = eval(config)[type].required;
 	console.log(filter.test(input));
 	return(filter.test(input));
+}
+
+//test lookup table
+var city_state_zip = [
+	{	"city" : "Agawam",
+		"state" : "MA",
+		"zip" : "01001"
+	},
+	{
+		"city" : "Amherst",
+		"state" : "MA",
+		"zip" : "01002"
+	},
+	{
+		"city" : "Barre",
+		"state" : "MA",
+		"zip" : "01005"
+	},
+	{
+		"city" : "Belchertown",
+		"state" : "MA",
+		"zip" : "01007"
+	},
+	{
+		"city" : "Blandford",
+		"state" : "MA",
+		"zip" : "01008"
+	}
+];		
+//lookup based on exhaustive
+function lkup_exhaustive(table,column,index){
+	var list = eval(table);
+	var result = list[0];
+	result.found = false;
+	for (var i = 0; i < list.length; i++){
+		if ((list[i])[column] == index){
+			result = list[i];
+			result.found = true;
+			break;
+		}
+	}
+	return result;
+}
+
+function lkup_binary(table,column,index){
+	var list = eval(table);	
+	var l_Index = 0;
+	var h_Index = list.length-1;
+	var m_Index;
+	var result = list[l_Index];
+	result.found = false;
+	//console.log("initial assignment: "+ result);
+	while (l_Index < h_Index -1) {
+    	m_Index = l_Index + Math.floor((h_Index - l_Index)/2);
+    	if (parseInt((list[m_Index])[column]) == parseInt(index)){
+    		result = list[m_Index];
+    		result.found = true;
+			break;
+    	} else if (parseInt((list[m_Index])[column]) < parseInt(index)) {
+    		l_Index = m_Index;
+    	} else {
+    		h_Index = m_Index;
+    	}
+    }	
+	return result;
 }
