f98d39d4ae49b781722d1d8e26365df4747cbde1
saeedt <saeed.tehrani@gmail.com>
2018-08-13 18:40:06 -0400

    Added multi-row binary and exhaustive lookup functions Added an object matching function

--- CFS_Validation_Platform/Saeed-test/js/code.js
+++ CFS_Validation_Platform/Saeed-test/js/code.js
@@ -55,7 +55,7 @@ function log(input){
 	//check_req_char("m.e@test.com","email","conf1");
 	//check_allowed_char("PtO1?", "shippingAdress", "conf1");
 	//console.log(lkup_exhaustive("city_state_zip","zip","01002"));
-	console.log(lkup_binary("city_state_zip","zip","01009"));
+	console.log(lkup_exhaustive_m("city_state_zip","zip","01001"));
 }
 
 //configuration object for verification functions
@@ -129,12 +129,12 @@ var city_state_zip = [
 	{
 		"city" : "Amherst",
 		"state" : "MA",
-		"zip" : "01002"
+		"zip" : "01001"
 	},
 	{
 		"city" : "Barre",
 		"state" : "MA",
-		"zip" : "01005"
+		"zip" : "01001"
 	},
 	{
 		"city" : "Belchertown",
@@ -144,10 +144,28 @@ var city_state_zip = [
 	{
 		"city" : "Blandford",
 		"state" : "MA",
-		"zip" : "01008"
+		"zip" : "01007"
 	}
-];		
-//lookup based on exhaustive
+];
+//matches a given object with an array of values
+function matchObj(ref,list,column){
+	for (var i=0; i<list.length; i++){
+		if ((list[i])[column] == ref[column]){
+			var match = true;
+			for (var key in list[i]){
+				if ((list[i])[key].toUpperCase() != ref[key].toUpperCase()){
+					match = false;
+					break;
+				}				
+			}
+			if (match) return true;			
+		}
+	} 
+	return false;
+}
+
+
+//lookup based on exhaustive search
 function lkup_exhaustive(table,column,index){
 	var list = eval(table);
 	var result = list[0];
@@ -161,7 +179,32 @@ function lkup_exhaustive(table,column,index){
 	}
 	return result;
 }
+//exhaustive lookup returning multiple matches
+function lkup_exhaustive_m(table,column,index){
+	var list = eval(table);
+	var result = new Object();
+	result.data = [];
+	result.data.push(list[0]);
+	result.found = false;
+	for (var i = 0; i < list.length; i++){
+		if ((list[i])[column] == index){
+			result.data[0] = list[i];
+			result.found = true;
+			var j = 1;
+    		while ((i+j)<list.length){
+    			if ((list[i+j])[column] == index){
+    				result.data.push(list[i+j]);
+        			j++;
+    			} else 
+    				break;
+    		}
+			break;
+		}
+	}
+	return result;
+}
 
+//lookup based on binary search
 function lkup_binary(table,column,index){
 	var list = eval(table);	
 	var l_Index = 0;
@@ -175,6 +218,47 @@ function lkup_binary(table,column,index){
     	if (parseInt((list[m_Index])[column]) == parseInt(index)){
     		result = list[m_Index];
     		result.found = true;
+			break;
+    	} else if (parseInt((list[m_Index])[column]) < parseInt(index)) {
+    		l_Index = m_Index;
+    	} else {
+    		h_Index = m_Index;
+    	}
+    }	
+	return result;
+}
+//binary lookup returning multiple matches
+function lkup_binary_m(table,column,index){
+	var list = eval(table);	
+	var l_Index = 0;
+	var h_Index = list.length-1;
+	var m_Index;
+	var result = new Object();
+	result.data = [];
+	result.data.push(list[l_Index]);
+	result.found = false;
+	//console.log("initial assignment: "+ result);
+	while (l_Index < h_Index -1) {
+    	m_Index = l_Index + Math.floor((h_Index - l_Index)/2);
+    	if (parseInt((list[m_Index])[column]) == parseInt(index)){
+    		result.data[0] = list[m_Index];
+    		result.found = true;
+    		var j = 1;
+    		while ((m_Index+j)<list.length){
+    			if (parseInt((list[m_Index+j])[column]) == parseInt(index)){
+    				result.data.push(list[m_Index+j]);
+        			j++;
+    			}else 
+    				break;    			
+    		}
+    		j = 1;
+    		while ((m_Index-j)>=0){
+    			if (parseInt((list[m_Index-j])[column]) == parseInt(index)){
+    				result.data.push(list[m_Index-j]);
+        			j++;
+    			}else 
+    				break;    			
+    		}    		
 			break;
     	} else if (parseInt((list[m_Index])[column]) < parseInt(index)) {
     		l_Index = m_Index;
