72066b5c839294c77dc0c1737bf196293016c714
hodarahmani <hodarahmani@192.168.100.11>
2018-09-26 17:37:24 -0400

    Renamed lookup tables

--- CFS_Validation_Platform/Hoda-test/js/rsc/lkup/lkup_5_28.js
+++ CFS_Validation_Platform/Hoda-test/js/rsc/lkup/lkup_5_28.js
@@ -121,3 +121,23 @@ var lkup20 = [
 	"maxWeight": infinity
 	}		
 ];
+
+var lkup21 = ["1","2","3"];
+
+var lkup22 = ["4","5","6"];
+
+var lkup23 = ["7","8","9"];
+
+var lkup24 = ["10","11","12"];
+
+var lkup25 = ["Y","N"];
+
+var lkup26 = ["7"];
+
+var lkup27 = ["1"];
+
+var lkup28 = ["8"];
+
+var lkup29 = ["2","3","4"];
+
+var lkup30 = ["33","42","45"];
