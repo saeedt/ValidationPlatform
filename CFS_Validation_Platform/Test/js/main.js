0da59c5e414e86e8855f3de9cdbfe66c64f64db2
saeedt <saeed.tehrani@gmail.com>
2018-11-06 18:20:09 -0500

    Updated the vpdemo GUI separated shipment and establishment file data validation added report download feature

--- CFS_Validation_Platform/Test/js/main.js
+++ CFS_Validation_Platform/Test/js/main.js
@@ -1,17 +1,21 @@
 var shipment;
 var company;
-var sfile_ok;
 var cfile_ok;
+var log_e;
+var log_s;
 //binding the event listener to the file picker button
 $(document).ready(function(e) {
-	document.getElementById('cfile').addEventListener('change', readFile_c, false);
+	document.getElementById('cfile').addEventListener('change', readFile_e, false);
 	document.getElementById('sfile').addEventListener('change', readFile_s, false);
-	document.getElementById("submit").disabled = true;
-	document.getElementById("submit").onclick = process;
+	document.getElementById("submit-s").disabled = true;
+	document.getElementById("submit-e").disabled = true;
+	document.getElementById("dl-report").disabled = true;
+	document.getElementById("submit-s").onclick = process_shp;
+	document.getElementById("submit-e").onclick = process_est;
+	document.getElementById("dl-report").onclick = download_report;
 });
 
 function readFile_s (evt) {
-	document.getElementById("submit").disabled = true;
     var file = evt.target.files[0];
     $("#sfiledetails").empty();   
     Papa.parse(file, {    	
@@ -28,8 +32,7 @@ function readFile_s (evt) {
     		comments: false,
     		step: undefined,
     		complete: function(result) {
-	    		shipment = result.data;
-	    		sfile_ok = false;
+	    		shipment = result.data;	    		
 	    		var html = [];
 	    		html.push('<p>Shipment data file<br>');
 	    		if (result.errors.length>0){
@@ -40,13 +43,10 @@ function readFile_s (evt) {
 	    				':'+result.errors[i].message);
 	    			}
 	    			html.push('Errors: <br>'+ errors.join("<br />")+'<br>');	
-	    		} else {
-	    			sfile_ok = true;
+	    		} else {	    			
 	    			html.push(result.data.length+' rows of data, '+result.meta.cursor+' characters <br>');
-	    			html.push('List of columns <br>'+ result.meta.fields.join("<br />")+ '<br>');
-	    			if (sfile_ok && cfile_ok){
-	    				document.getElementById("submit").disabled = false;
-	    			}
+	    			html.push('List of columns <br>'+ result.meta.fields.join("<br />")+ '<br>');	    			
+	    			document.getElementById("submit-s").disabled = false;	    			
 	    		}
 	    		$('#sfiledetails').append(html.join('')+'</p>');    		
     		},
@@ -60,15 +60,15 @@ function readFile_s (evt) {
     });
  }
 
-function readFile_c (evt) {
-	document.getElementById("submit").disabled = true;
+function readFile_e (evt) {
     var file = evt.target.files[0];
     var fr = new FileReader();
     fr.onload = function () {
     	var result;
     	cfile_ok = false;
     	var html = [];
-    	html.push('<p>Company data file<br>') ;
+    	var log_e = [];
+    	html.push('<p>Establishment data file<br>') ;
     	$('#cfiledetails').empty();
     	try {
     		result = JSON.parse(fr.result);
@@ -83,8 +83,8 @@ function readFile_c (evt) {
     		html.push('Errors: '+e+'<br>');
     		cfile_ok = false;
     	}
-    	if (sfile_ok && cfile_ok){
-			document.getElementById("submit").disabled = false;
+    	if (cfile_ok){
+			document.getElementById("submit-e").disabled = false;
 		}
     	html.push('</p>');
     	$('#cfiledetails').append(html.join(''));
@@ -92,34 +92,51 @@ function readFile_c (evt) {
     fr.readAsText(file);        
  }
 
-function process(){
-	var cresult = verify_company(company);
-	var sresult = verify_shipment(shipment);
-	$("#log-content").empty();
+function process_est(){
+	var cresult = verify_company(company);	
+	$("#log-est").empty();
 	var pass = true;
 	var html = [];
+	log_e = [];
 	html.push('<p> Establishment data file validation<br>');
+	log_e.push('Establishment data file validation\n');
 	Object.keys(cresult).forEach(function(key,index) {
 		if (cresult[key].flags.length>0) {
 			pass = false;
 			html.push(key+'<br>');
+			log_e.push(key+'\n');
 			for (var i=0; i<cresult[key].flags.length; i++){
 				html.push(cresult[key].flags[i]+'='+cresult[key].flagval[i]+' '+cresult[key].flagname[i]+' '+cresult[key].flagmsg[i]+'<br>');
+				log_e.push('\t'+cresult[key].flags[i]+'='+cresult[key].flagval[i]+' '+cresult[key].flagname[i]+' '+cresult[key].flagmsg[i]+'\n');
 			}
 		}		
 	});
-	if (pass)
-		html.push('No errors<br>');
-	pass = true;
-	html.push('</p><hr>');
+	if (pass){
+		html.push('No error found in the establishment data file<br>');
+		log_e.push('No error found in the establishment data file\n');	
+	}		
+	html.push('</p>');
+	$("#log-est").append(html.join(''));
+	document.getElementById("dl-report").disabled = false;	
+}
+
+function process_shp(){
+	var sresult = verify_shipment(shipment);
+	$("#log-shp").empty();
+	var pass = true;
+	var html = [];
+	log_s = [];
 	html.push('<p> Shipment data file validation<br>');
+	log_s.push('Shipment data file validation\n');
 	for (var i=0; i<sresult.length-1; i++){
 		Object.keys(sresult[i]).forEach(function(key,index) {
 			if (sresult[i][key].flags.length>0) {
 				html.push(key+'<br>');
+				log_s.push(key+'\n');
 				pass = false;
 				for (var j=0; j<sresult[i][key].flags.length;j++){
 					html.push('(Line '+ String(i+1)+') '+sresult[i][key].flags[j]+'='+sresult[i][key].flagval[j]+' '+sresult[i][key].flagname[j]+' '+sresult[i][key].flagmsg[j]+'<br>');
+					log_s.push('\t(Line '+ String(i+1)+') '+sresult[i][key].flags[j]+'='+sresult[i][key].flagval[j]+' '+sresult[i][key].flagname[j]+' '+sresult[i][key].flagmsg[j]+'\n');
 				}
 			}
 		});				
@@ -127,10 +144,23 @@ function process(){
 	if (sresult[sresult.length-1].flags.length >0){
 		pass = false;
 		html.push(sresult[sresult.length-1].flags[0]+'='+sresult[sresult.length-1].flagval[0]+' '+sresult[sresult.length-1].flagname[0]+' '+sresult[sresult.length-1].flagmsg[0]+'<br>');
+		log_s.push(sresult[sresult.length-1].flags[0]+'='+sresult[sresult.length-1].flagval[0]+' '+sresult[sresult.length-1].flagname[0]+' '+sresult[sresult.length-1].flagmsg[0]+'\n');
 	}
-	if (pass)
-		html.push('No errors<br>');
+	if (pass){
+		html.push('No error found in the shipment data file<br>');
+		log_s.push('No error found in the shipment data file\n');
+	}		
 	html.push('</p>');
-	$("#log-content").append(html.join(''));
-	
+	$("#log-shp").append(html.join(''));
+	document.getElementById("dl-report").disabled = false;
+}
+
+function download_report(){
+	var output = log_e.concat(log_s);
+	var a         = document.createElement('a');
+	a.href        = 'data:attachment/txt,' + encodeURIComponent(output.join(''));
+	a.target      = '_blank';
+	a.download    = 'Report.txt';
+	document.body.appendChild(a);
+	a.click();
 }
