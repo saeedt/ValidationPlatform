443e20a70dbd87ef596f1cd6459a13239c4a0325
Saeed Tehrani <saeed.tehrani@gmail.com>
2019-05-24 19:30:16 -0400

    User configurable variable names (WIP) Added the variable configuration objects Added functions to match and score variable names based on similarity

---
+++ CFS_Validation_Platform/Test/js/rsc/variables.js
@@ -0,0 +1,156 @@
+var shp = {
+		1: {
+			name: 'Ship_Num',
+			keys: ['ship','shp','num','key','id','unique','identification','cargo','package','product','consignment']
+		},
+		2: {
+			name: 'Ship_Month',
+			keys: ['ship','shp','month','delivery']
+		},
+		3: {
+			name: 'Ship_Day',
+			keys: ['ship','shp','day','delivery']
+		},
+		4: {
+			name: 'Ship_value',
+			keys: ['ship','shp','val','order','amount','cost','rate','price','merchandise','consignment','package','parcel']
+		},
+		5: {
+			name: 'Ship_Weight',
+			keys: ['ship','shp','weight','wgt','tonnage','order','product','merchandise','consignment','package','parcel','gross','cargo','inbound','outbound','commodity','freight']
+		},
+		6: {
+			name: 'SCTG_Commodity_Code',
+			keys: ['sctg','commodity','code']
+		},
+		7: {
+			name: 'Commodity_Description',
+			keys: ['commodity','desc','product','ship','consignment','cargo']
+		},
+		8: {
+			name: 'Temp_Control_YN',
+			keys: ['temp','control','ctrl']
+		},
+		9: {
+			name: 'HAZMAT_Code',
+			keys: ['hazmat','hazardous','material','dangerous','control','ctrl']
+		},
+		10: {
+			name: 'Domestic_City_Name',
+			keys: ['city','name','domestic','town']
+		},
+		11: {
+			name: 'Domestic_State_Abbrev',
+			keys: ['State','domestic','abbr','name']
+		},
+		12: {
+			name: 'Domestic_ZIP_Code',
+			keys: ['zip','code','domestic','post']
+		},
+		13: {
+			name: 'Domestic_Transport_Mode',
+			keys: ['mode','freight','domestic','transport','tsp']
+		},
+		14: {
+			name: 'Export_YN',
+			keys: ['export','yn']
+		},
+		15: {
+			name: 'Export_City_Name',
+			keys: ['export','city','name','town']
+		},
+		16: {
+			name: 'Export_Country_Name',
+			keys: ['export','country','name']
+		},
+		17: {
+			name: 'Export_Transport_Mode',
+			keys: ['export','transport','mode','transit','tsp']
+		}	
+}
+var est = {
+		1: {
+			name: 'Mailing_Address',
+			keys: ['mail','add']
+		},
+		2: {
+			name: 'Mailing_Attention',
+			keys: ['mail','attention','attn']
+		},
+		3: {
+			name: 'Mailing_City',
+			keys: ['mail','city','town']
+		},
+		4: {
+			name: 'Mailing_Comapny_Name_1',
+			keys: ['mail','company','name','1']
+		},
+		5: {
+			name: 'Mailing_Comapny_Name_2',
+			keys: ['mail','company','name','2']
+		},
+		6: {
+			name: 'Mailing_State',
+			keys: ['mail','state']
+		},
+		7: {
+			name: 'Mailing_Zip4',
+			keys: ['mail','zip','4','dig']
+		},
+		8: {
+			name: 'Mailing_Zip5',
+			keys: ['mail','zip','5','dig']
+		},
+		9: {
+			name: 'Shipment_Number',
+			keys: ['ship','num','total','merchandise','consignment','package','parcel']
+		},
+		10: {
+			name: 'Shipment_Value',
+			keys: ['ship','shp','val','total','amount','cost','rate','price','merchandise','consignment','package','parcel']
+		},
+		11: {
+			name: 'Shipping_Address',
+			keys: ['ship','shp','add']
+		},
+		12: {
+			name: 'Shipping_City',
+			keys: ['ship','shp','city','town']
+		},
+		13: {
+			name: 'Shipping_Campany_Name_1',
+			keys: ['ship','shp','company','name','1']
+		},
+		14: {
+			name: 'Shipping_Campany_Name_2',
+			keys: ['ship','shp','company','name','2']
+		},
+		15: {
+			name: 'Shipping_State',
+			keys: ['ship','shp','state']
+		},
+		16: {
+			name: 'Shipping_Zip4',
+			keys: ['ship','shp','zip','4','dig']
+		},
+		17: {
+			name: 'Shipping_Zip5',
+			keys: ['ship','shp','zip','5','dig']
+		},
+		18: {
+			name: 'Status_Ceased_Day',
+			keys: ['stat','cease','day']
+		},
+		19: {
+			name: 'Status_Ceased_Month',
+			keys: ['stat','cease','month']
+		},
+		20: {
+			name: 'Status_Ceased_Year',
+			keys: ['stat','cease','year','yr']
+		},
+		21: {
+			name: 'Status_In_Operation',
+			keys: ['stat','in','opr','operation']
+		}
+	};
