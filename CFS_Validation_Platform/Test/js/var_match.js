c176b69354a2eb4d4570a7e3378b494f57bbdda2
Saeed Tehrani <saeed.tehrani@gmail.com>
2019-06-03 19:25:06 -0400

    User Configurable Variable Names (WIP)

--- CFS_Validation_Platform/Test/js/var_match.js
+++ CFS_Validation_Platform/Test/js/var_match.js
@@ -1,28 +1,52 @@
-function evalVar(input,type){
+//Matches each STD attribute names with the list of user input variables 
+//input:array of strings - user attributes
+function evalVars(input,type){ 
 	var list = eval(type);
 	var score;
-	var result = new Object();
-	result.input = input;
-	result.eval = [];
-	var scr; 
+	var tmp;
+	var scr;
+	var result = [];	 
 	for (var id in list){
-		score = 0;
-		scr = new Object();		
-		for (var kwd of list[id].keys){			
-			if (input.toLowerCase().includes(kwd.toLowerCase())){
-				score++;
-			}				
+		tmp = new Object();
+		tmp.id = id;
+		tmp.varname = list[id].name;
+		tmp.eval = [];
+		for (var i=0; i<input.length; i++){
+			score = 0;
+			scr = new Object();		
+			for (var kwd of list[id].keys){			
+				if (input[i].toLowerCase().replace(/\s+/g,'').includes(kwd.toLowerCase())){
+					score++;
+				}				
+			}
+			scr.id = i;
+			scr.name = input[i];
+			scr.score = score;
+			tmp.eval.push(scr);
+		}	
+		tmp.eval.sort(compare);
+		if (tmp.eval[0].score > tmp.eval[1].score){
+			tmp.match = true;		
+		} else {
+			tmp.match = false;
 		}
-		scr.id = id;
-		scr.name = list[id].name;
-		scr.score = score;
-		result.eval.push(scr);
-	}	
-	result.eval.sort(compare);
-	if (result.eval[0].score > result.eval[1].score){
-		result.match = true;
-	} else {
-		result.match = false;
+		result.push(tmp);
+	}
+	for (var i=0; i<reslt.length; i++){	
+		if (result[i].match){
+			for (var j=0; j<result.length; j++){
+				if (result[j].match && result[i].eval[0].id == result[j].eval[0].id){
+					if (result[i].eval[0].score > result[j].eval[0].score){
+						result[j].match = false;
+					} else if (result[i].eval[0].score < result[j].eval[0].score){
+						result[i].match = false;
+					} else {
+						result[i].match = false;
+						result[j].match = false;
+					}
+				}				
+			}							
+		}
 	}
 	return result;
 }
@@ -37,44 +61,64 @@ function compare(a, b) {
   return 0;
 }
 //generates the combobox for a variable id
-function getCombo(input,id,bind){
-	var buffer = '<select id="'+id+'">';
-	if (input.bind){
-		buffer += '<option value ="NA">Make a selection</option>';
-	}
-	for (var scr of input.score){
-		buffer += '<option value ="'+ scr.id +'">'+ scr.name + '</option>';
-	}
+function getCombo(input, type){
+	var buffer = '<select id="'+type+input.id+'">';
+	if (!input.match){
+		buffer += '<option value ="-1" selected disabled hidden>Make a selection</option>';
+		for (var i=0; i<input.eval.length; i++){
+			buffer += '<option value ="'+ input.eval[i].id +'">'+ input.eval[i].name + '</option>';
+		}
+	} else {
+		buffer += '<option value ="'+input.eval[i].id+'" selected>'+ input.eval[0].name + '</option>';
+		for (var i=1; i<input.eval.length; i++){
+			buffer += '<option value ="'+ input.eval[i].id +'">'+ input.eval[i].name + '</option>';
+		}
+	}	
 	buffer+= '</select>';
 }
 //takes an array of attribute names, matches them individually with the variable names and returns an object with matched variable names and combo boxes
-function evalVars(input,type){
-	var result = new object();
-	var varray = [];
-	var tmp;	
-	for (var field of input){
-		varray.push(evalVar(field,type));		
-	}
-	for (var field of type){
+//input is a string array
+function processVars(input,type){
+	var result = evalVars(input,type);		
+	for (var i=0; i<input.length; i++){
+		input[i].combo = getCombo(input[i]);	
+	}	
+	return result;
+}
+//verifies the variable configuration and returns the updated mapping
+function verify_combos(type){
+	var list = eval(type);
+	var result = new Object();
+	var valid = true;
+	var vars = [];
+	var tmp;
+	for (var id in list)){
 		tmp = new Object();
-		
+		tmp.id = id;
+		tmp.name = list[id].name;
+		tmp.match = true;
+		for 
 	}
 }
-//Checks whether multiple attributes match the same variable or not
-// we run evalvar on individual attributes first, then check them all together
-function findVar(id,input){
-	var maxScore = 0;
-	var maxIndex = -1;
+
+//Checks whether multiple attributes are matched with the same variable 
+//input: array of objs from evalvar() 
+/*function corssChkVars(input){	//No longer needed - merged with evalvars
 	for (var i=0; i<input.length; i++){	
-		if (input[i].match){			
-			if (input[i].eval[0].id == id && input[i].eval[0].id > maxScore){
-				maxIndex = i;
-				maxScore = input[i].eval[0].score;
-			} else if (input[i].eval[0].id == id && input[i].eval[0].id == maxScore){
-				maxIndex = -1;
-				break;
-			}				
+		if (input[i].match){
+			for (var j=0; j<input.length; j++){
+				if (input[j].match && input[i].eval[0].id == input[j].eval[0].id){
+					if (input[i].eval[0].score > input[j].eval[0].score){
+						input[j].match = false;
+					} else if (input[i].eval[0].score < input[j].eval[0].score){
+						input[i].match = false;
+					} else {
+						input[i].match = false;
+						input[j].match = false;
+					}
+				}				
+			}							
 		}
-	}
-	return maxIndex;	
-} 
+	}	
+	return input;	
+}*/
