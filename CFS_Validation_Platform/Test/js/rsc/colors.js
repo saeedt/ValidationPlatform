b5b481a157a8f87437a1e35bb987a9da9299c57b
Saeed Tehrani <saeed.tehrani@gmail.com>
2019-06-19 17:44:22 -0400

    Added table row colors to PDF and Excel exported files

--- CFS_Validation_Platform/Test/js/rsc/colors.js
+++ CFS_Validation_Platform/Test/js/rsc/colors.js
@@ -1,12 +1,12 @@
 var colors = {
 	1:{
-		color: "rgba(255, 0, 0, 0.2)"	
+		color: "rgba(255, 0, 0, 0.2)"	// #ffcccc rgb(255,204,204)
 	},
 	2:{
-		color: "rgba(255,153,0,0.4)"	
+		color: "rgba(255,153,0,0.4)"	// #ffd699 rgb(255,214,153)
 	},
 	3:{
-		color: "rgba(255, 255, 0, 0.2)"	
+		color: "rgba(255, 255, 0, 0.2)"	 //   #ffffcc rgb(255,255,204)
 	}	
 }
 
